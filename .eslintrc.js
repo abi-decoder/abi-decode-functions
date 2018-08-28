036bc9fbe17ae0f3d335b3d153643bb0578ab602
Yukishige Nakajo <nakajo@haw.co.jp>
2018-08-28 15:42:55 +0900

    prepare environment.

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: 'standard',
+  plugins: ['mocha'],
+  parserOptions: {
+    'ecmaVersion':2017
+  },
+  env: {'mocha': true},
+  rules: {
+    'space-before-function-paren': ['error', 'never'],
+    'no-underscore-dangle': 0,
+    'mocha/no-exclusive-tests': 'error'
+  }
+}
