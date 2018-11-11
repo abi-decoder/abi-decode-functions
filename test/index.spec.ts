c094cdca07c1501bae55bb74c50417e40fc446c2
Yukishige Nakajo <nakajo@haw.co.jp>
2018-11-12 02:10:32 +0900

    change: Change Module to Class. change: dist is created when publish.

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,9 +1,13 @@
 /*tslint space-after-keywords: [2, "never"] */
 import { expect } from 'chai'
-import * as decoder from '../src/index'
+import AbiFunctions from '../src/index'
 
 describe('index.js', () => {
   describe('isFunctionSelector', () => {
+    let decoder: any
+    beforeEach(() => {
+      decoder = (new AbiFunctions('') as any)
+    })
     it('true', async () => {
       const data = [
         { name: 'DUP1', pc: 54 },
@@ -37,6 +41,10 @@ describe('index.js', () => {
   describe('parseCode', () => {
     const footerMetadata = '00a165627a7a723058208e0087b75d0ea5908b0776e3657eca75d4d0052a722dd2d3c6a4ad6236b359e90029'
     const opcodes = '0x608060405260043610610078576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff1680632c0222891461007d57'
+    let decoder: any
+    beforeEach(() => {
+      decoder = (new AbiFunctions('') as any)
+    })
     it('success', async () => {
       const result = decoder._parseCode(opcodes + footerMetadata)
       expect(result).to.be.a('array')
@@ -55,17 +63,20 @@ describe('index.js', () => {
     const footerMetadata = '00a165627a7a723058208e0087b75d0ea5908b0776e3657eca75d4d0052a722dd2d3c6a4ad6236b359e90029'
     const opcodes = '0x608060405260043610610078576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff1680632c0222891461007d57'
     it('success', async () => {
-      const result = decoder.getFunctionIds(opcodes + footerMetadata)
+      const decoder = new AbiFunctions(opcodes + footerMetadata)
+      const result = decoder.getFunctionIds()
       expect(result).to.be.a('array')
       expect(result).to.have.length(1)
       expect(result[0]).to.equal('0x2c022289')
     })
     it('return empty result when invalid args', async () => {
-      const result = decoder.getFunctionIds('hogehoge')
+      const decoder = new AbiFunctions('hogehoge')
+      const result = decoder.getFunctionIds()
       expect(result).to.have.length(0)
     })
     it('return empty result when not contains function selector', async () => {
-      const result = decoder.getFunctionIds('0x1234567890abcdef' + footerMetadata)
+      const decoder = new AbiFunctions('0x1234567890abcdef' + footerMetadata)
+      const result = decoder.getFunctionIds()
       expect(result).to.have.length(0)
     })
   })
